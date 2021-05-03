1495cf89d2095206c99d2d5e96de432457291537
Bui Gia An <buigiaanfb1@gmail.com>
2021-05-03 17:02:38 +0700

    fixed unavailable music API

--- src/data.js
+++ src/data.js
@@ -1,70 +1,38 @@
 import { v4 as uuidv4 } from "uuid";
+import saveMe from "./saveMe.mp3";
 function chillHop() {
   return [
     {
-      name: "Có làm có ăn",
-      cover:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROtRexjLK8flJ1PQ06XeJz99L_AVrKmDnC4eDByD67UJk_XJ7KIGh5XrGra6iS-Siovpk&usqp=CAU",
-      artist: "Huấn Hoa Hồng",
-      audio: "./music/test.mp3",
-      color: ["#205950", "#2ab3bf"],
-      id: uuidv4(),
-      active: true,
-    },
-    {
-      name: "Save me",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
-      artist: "DREAMN",
+      name: "Despacito",
+      cover: "https://i1.sndcdn.com/artworks-000228539829-7wxbpg-t500x500.jpg",
+      artist: "Luis Fonsi, Daddy Yankee, Justin Bieber",
       audio:
-        "https://vnso-zn-5-tf-mp3-320s1-zmp3.zadn.vn/87b97661a6254f7b1634/8646980269950432717?authen=exp=1613749205~acl=/87b97661a6254f7b1634/*~hmac=5f9ad60126be7f3669c79bf6d7464250&fs=MTYxMzU3NjQwNTQxNHx3ZWJWNnwxMDM3OTEwNTM3fDE0LjE4Ny4xMjYdUngMTEy",
-      color: ["#EF8EA9", "#ab417f"],
+        "https://c1-ex-swe.nixcdn.com/Unv_Audio83/DespacitoRemix-LuisFonsiDaddyYankeeJustinBieber-5443166.mp3?st=lrCgcnGOuEJ9adD43JJXtQ&e=1613805210",
+      color: ["#9A9A9A", "#242424"],
       id: uuidv4(),
       active: false,
     },
     {
-      name: "Sing me to sleep",
+      name: "Young And Beautiful",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-      artist: "Alan Walker",
+        "https://lastfm.freetls.fastly.net/i/u/770x0/b5016415cb8840189a44eaf0845912f7.jpg",
+      artist: "Lana Del Rey",
       audio:
-        "https://vnso-zn-15-tf-mp3-320s1-zmp3.zadn.vn/569d76aa20edc9b390fc/1724142149151916628?authen=exp=1613809428~acl=/569d76aa20edc9b390fc/*~hmac=2472f7e9f3d26375c22f9e68b0edc1d0&fs=MTYxMzYzNjYyODgwNXx3ZWJWNnwxMDAxODI4ODQ5fDE0LjE3OS4yMTEdUngNjE",
-      color: ["#CD607D", "#c94043"],
+        "https://c1-ex-swe.nixcdn.com/Unv_Audio12/YoungAndBeautiful-LanaDelRey-2508785.mp3?st=Dgj9dF_7k9ehhwyhAyszdw&e=1613805686&t=1613719286283",
+      color: ["#9D2C56", "#693E51"],
       id: uuidv4(),
       active: false,
     },
     {
-      name: "Nightfall",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
-      artist: "Aiguille",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
-      color: ["#EF8EA9", "#ab417f"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Reflection",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-      artist: "Swørn",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
-      color: ["#CD607D", "#c94043"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Under the City Stars",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-      artist: "Aso, Middle School, Aviino",
+      name: "Victory",
+      cover: "https://i1.sndcdn.com/artworks-000352681869-jwswel-t500x500.jpg",
+      artist: "Two Step From Hell",
       audio:
-        "https://c1-ex-swe.nixcdn.com/NhacCuaTui1010/TinhBanDieuKy-AMeeRickyStarLangLD-6927558.mp3?st=ErrHjlxQoc5zSPNzrFGhtQ&e=1613723335",
-      color: ["#205950", "#2ab3bf"],
+        "https://c1-ex-swe.nixcdn.com/NhacCuaTui894/Victory-TwoStepsFromHell-3890867.mp3?st=NneSVR5dxnXQMB3qrhI_wQ&e=1613805974&t=1613719574340",
+      color: ["#FFE0A3", "#40607F"],
       id: uuidv4(),
       active: false,
     },
-    //ADD MORE HERE
   ];
 }
 
